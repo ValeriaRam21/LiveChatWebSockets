ccac86bbb8892b4fe30c80c361cdabaa1b98b764
JaimeJuarez <jjuarez17@ucol.mx>
2022-04-25 14:28:42 -0500

    Second Commit

--- app.js
+++
@@ -1,8 +0,0 @@
-let btnSend = document.getElementById('sendmsg')
-let inputName = document.getElementById('name').value
-const changeName = () => {
-    let nombre = inputName
-    console.log(nombre);
-}
-
-// inputName.addEventListener('click', changeName);
